6e5f58ec1a35816583834d35d0dd1e4cdd230506
Samuel Abiodun <samuelabiodun4square@gmail.com>
2022-02-07 01:06:54 +0100

    ADDED FINISHED AND COMPLETED UI
    I added finished and completed UI for the game using a panel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static bool gameOver;
     public static bool levelComplete;
+    public GameObject gameOverPanel;
+    public GameObject levelCompletedPanel;
 
     // Start is called before the first frame update
     void Start()
     {
         gameOver = levelComplete = false;
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
@@ -19,6 +23,22 @@ public class GameManager : MonoBehaviour
         if (gameOver)
         {
             Time.timeScale = 0;
+            gameOverPanel.SetActive(true);
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                SceneManager.LoadScene("Level");
+            }
+        }
+
+        if (levelComplete)
+        {
+            levelCompletedPanel.SetActive(true);
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                SceneManager.LoadScene("Level");
+            }
         }
     }
 }
